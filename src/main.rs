3e39405b010a341ac64073516046b2649b8e7e13
Finkch <skyler@thealdersons.org>
2024-12-28 11:31:38 -0800

    Added a function to apply gravity between pairs of actors

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,22 @@ mod vector3;
 use actor::Actor;
 
 fn main() {
-    println!("Hello, world!");
-
 
     let actor = Actor::new("Terra", 1e4, 1e4);
 
     println!("{}", actor);
+}
+
+
+// Applies gravity between a pair of actors
+fn gravity(a: &mut Actor, b: &mut Actor) {
+
+    let g = 6.67430e-11;
+
+    let r = a.pos - b.pos;
+
+    let f = g / r.mag().powi(3) * r;
+
+    a.acc = a.acc + f * b.mass;
+    b.acc = b.acc - f * a.mass;
 }
