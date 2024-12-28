cb9d6397f9bb44e5c41ae577f7ea84d8a63dadd2
Finkch <skyler@thealdersons.org>
2024-12-28 11:45:03 -0800

    Added a file to contain initial solar system states

---
+++ src/solarsystems.rs
@@ -0,0 +1,42 @@
+// A place to store various solar systems
+
+use actor::Actor;
+
+const AU: f64 = 1.496e11;
+const G:  f64 = 6.67430e-11;
+
+const MS: f64 = 1.989e30;
+const RS: f64 = 696340e3;
+
+const ME: f64 = 5.972e24;
+const RE: f64 = 6378e3;
+
+//      Actor parameters
+// pub name: String,
+// pub mass: f64,
+// pub radius: f64,
+
+fn vis_viva(parent: &Actor, r: f64) -> f64 {
+    (G * parent.mass / r).sqrt()
+}
+
+// Simple sun-earth system
+pub fn sol_terra() -> [Actor; 2] {
+    let actors = [
+        Actor {
+            name: "Sol",
+            mass: MS,
+            radius: RS,
+        },
+        Actor{
+            name: "Terra",
+            mass: ME,
+            radius: RE,
+        },
+    ];
+
+    actors[1].pos.x = AU;
+    actors[1].vel.y = vis_viva(actors[0], AU);
+
+    return actors;
+}
